9767122a4d75215ddb80474f45d47a1d506556a1
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-02-24 19:30:13 -0600

    uninstall react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import GlobalNavbar from "./components/GlobalNavbar";
+import Navbar from "./components/Navbar";
 import Index from "./components/Index";
 import NotFound from "./components/NotFound";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <>
-      <GlobalNavbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
--- src/components/Index.js
+++ src/components/Index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
 
 export default function Index() {
   return (
     <>
-      <Container>
-        <h1>Welcome!</h1>
-      </Container>
+      <h1>Welcome!</h1>
     </>
   );
 }
